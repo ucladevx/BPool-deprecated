b981dcc50ec9a05286b3390ad5ef98054ff8e0b2
shannonphu <shannonphu@sbcglobal.net>
2017-10-31 21:46:22 -0700

    reordered index.js

--- node_app/index.js
+++ node_app/index.js
@@ -1,19 +1,18 @@
+// create a server and listen on settings.config.port
 const settings = require('./app');
-// create a server and listen on app.config.port
-
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
+app.listen(settings.config.port, () => {
+	console.log("Listening on port 3000");
+});
+
 app.get('/', (req, res) => {
 	res.render('home', {
 		title: "Home",
 		content: "Hello world"
 	});
-});
-
-app.listen(settings.config.port, () => {
-	console.log("Listening on port 3000");
 });
