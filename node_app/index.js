af14d6e640ffbb8cf8c7705f4df79ef5623c3d21
shannonphu <shannonphu@sbcglobal.net>
2017-10-31 20:40:03 -0700

    setup hello world server and main view

--- node_app/index.js
+++ node_app/index.js
@@ -1,2 +1,19 @@
-const app = require('./app');
+const settings = require('./app');
 // create a server and listen on app.config.port
+
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('home', {
+		title: "Home",
+		content: "Hello world"
+	});
+});
+
+app.listen(settings.config.port, () => {
+	console.log("Listening on port 3000");
+});
