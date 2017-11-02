e33862486afe79d490c991b299e5e06bdb2b9a80
Garchbold <garchbold@g.ucla.edu>
2017-11-01 21:47:29 -0700

    rerouted file path to port config

--- node_app/index.js
+++ node_app/index.js
@@ -1,12 +1,12 @@
 // create a server and listen on settings.config.port
-const settings = require('./app');
+const settings = require('./config.js');
 const express = require('express');
 const app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
-app.listen(settings.config.port, () => {
+app.listen(settings.port, () => {
 	console.log("Listening on port 3000");
 });
 
