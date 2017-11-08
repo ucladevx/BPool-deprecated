903566443788f9412052f144e6626a7fd9e34dbc
Shannon Phu <shannonphu@sbcglobal.net>
2017-11-07 23:47:31 -0800

    Implemented dashboard to create new ride (#4)
    * made form UI for /ride/new to create new ride
    
    * added POST endpoint for creating rides from /ride/create form

--- node_app/index.js
+++ node_app/index.js
@@ -3,6 +3,7 @@ const settings = require('./config.js');
 const express = require('express');
 const app = express();
 const exphbs = require('express3-handlebars');
+const bodyParser = require('body-parser');
 
 // Database Models
 const db = require('./db/db.js');
@@ -17,6 +18,8 @@ app.engine( 'hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(settings.port, () => {
 	console.log("Listening on port 3000");
 });
@@ -25,4 +28,23 @@ app.get('/', (req, res) => {
 	res.render('home', {
 		title: "Home"
 	});
+});
+
+app.get('/ride/new', (req, res) => {
+	res.render('create_ride');
+});
+
+app.post('/ride/create', (req, res) => {
+	let rideDate = req.body.date;
+	let rideTime = req.body.time;
+	let rideOrigin = req.body.origin;
+	let rideDestination = req.body.destination;
+	let carModel = req.body.carModel;
+	let carNumSeats = req.body.carNumSeats;
+	let rideDescription = req.body.rideDescription;
+	let ridePrice = req.body.price;
+
+	// TODO: Create and save Ride object to database
+
+	res.redirect('/');
 });
