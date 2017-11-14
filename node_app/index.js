126abb7e8a1205c202f9cc708ad9b98a7ebd51e7
samarseth <samarseth@aol.com>
2017-11-14 11:25:00 -0800

    Added fb auth
    Used Passport JS to add fb auth

--- node_app/index.js
+++ node_app/index.js
@@ -4,7 +4,11 @@ const express = require('express');
 const app = express();
 const exphbs = require('express3-handlebars');
 const bodyParser = require('body-parser');
-
+/* Start Code Change*/
+const session = require('express-session');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook');
+/* End Code Change*/
 // Database Models
 const db = require('./db/db.js');
 const User = require('./db/user.js');
@@ -19,6 +23,37 @@ app.engine( 'hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
+/* Start Code Change*/
+
+var facebook_id = '125963121407912';
+var facebook_secret_id = '867830b1fd7c4f910c171f06a1c0d3c7';
+
+var fbObject = {
+	clientID: facebook_id,
+	clientSecret: facebook_secret_id,
+	callbackURL: 'http://localhost:3000/auth/facebook/callback',
+	profileFields: ['emails ']
+}
+
+var fbCallback = function(accessToken, refreshToken, profile, cb) {
+	console.log(accessToken, refreshToken, profile);
+};
+
+passport.use(new FacebookStrategy(fbObject, fbCallback));
+
+app.route('/auth')
+	.get(passport.authenticate('facebook', {scope: ['email']}));
+
+app.route('/auth/facebook/callback')
+	.get(passport.authenticate('facebook', function(err, user, info) {
+			console.log(err, user, info)
+			// save the data to a database (mongodb)
+	})
+);
+
+/* End Code Change*/
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(settings.port, () => {
@@ -30,6 +65,7 @@ app.get('/', (req, res) => {
 		title: "Home"
 	});
 });
+
 
 app.get('/ride/new', (req, res) => {
 	res.render('create_ride');
