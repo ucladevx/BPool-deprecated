115fe95cb6395d78358ffef2a086c73d85542d0b
Aravind Sripathy <aravindsripathy@s-169-232-126-229.resnet.ucla.edu>
2017-11-14 17:53:24 -0800

    Addressed pr comments

--- node_app/db/ride.js
+++ node_app/db/ride.js
@@ -13,66 +13,39 @@ var rideSchema = new mongoose.Schema({
 
 /*
  Functionality- Searches for all rides that match passed in criteria
-                Destination - required
-                Source - required
-                Price - Maximum Price willing to pay (default 100)
-                Day - Day of Month looking to travel (1-31)
-                Month - Month looking to travel (1-12) - required
+                Destination
+                Source
+                Date - Date object specifying month and day
  Usage:
-   Ride.searchByFilters("Destination", "Source", Price, Day, Month, (rides) => {
+ //Month is (0-11) and Day is (1-31)
+   Ride.searchByFilters("Destination", "Source", new Date((new Date()).getUTCFullYear(), month, day), (rides) => {
      console.log(rides);
    });
-   Pass in null if no filter provided
 Returns:
   All rides in db that match criteria
 */
 
-rideSchema.statics.searchByFilters = function(destination, source, price, day, month, callback) {
+rideSchema.statics.searchByFilters = function(destination, source, date, callback) {
   var query =
   {
     'destination' : destination,
     'source': source
   }
+  //Given a Date Object specifying Month and Day, creates two new date objects
+  //representing start and end of day. Then matches with all rides whose date
+  //takes place within that time frame.
+  var startOfDate = (new Date(date.getTime()));
+  var endOfDate = (new Date(date.getTime()));
+  endOfDate.setHours(24);
 
-  var maxPrice
-  if(price){
-    maxPrice = price
-  }
-  else{
-    maxPrice = 100;
-  }
-
-  //Month is 1 based
-  function daysInMonth(month, year) {
-      return new Date(year, month, 0).getDate();
-  }
-
-  var date = new Date();
-  var startDay;
-  var endDay
-  if(day){
-    startDay = day;
-    endDay = day;
-  }
-  else{
-    startDay = 1;
-    endDay = daysInMonth(month, date.getUTCFullYear());
-  }
-
-  var startTime = new Date(date.getUTCFullYear(), month - 1, startDay);
-  var endTime = new Date(date.getUTCFullYear(), month - 1, endDay, 11, 59, 59, 0);
-
-  Ride.find(query).where('price').lt(maxPrice).where('date').gt(startTime).lt(endTime).exec(function(err, rides){
-    if(err)
+  Ride.find(query).where('date').gt(startOfDate).lt(endOfDate).exec(function(err, rides){
+    if (err) {
       throw err;
-      if (callback) {
-  			callback(rides);
-  		}
-  })
-}
-
-rideSchema.remove = function(callback) {
-  ride.remove({});
+    }
+    if (callback) {
+      callback(rides);
+  	}
+  });
 }
 
 /*
@@ -97,7 +70,7 @@ rideSchema.statics.insert = function(source, destination, price, date, driver, c
 			throw err;
 		}
 		if (callback) {
-			callback(ride);
+      callback(ride);
 		}
 		return;
 	});
