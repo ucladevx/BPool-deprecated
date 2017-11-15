158e53ff21eaa2e19828c0a504b9cfa510131aca
shannonphu <shannonphu@sbcglobal.net>
2017-11-14 16:55:50 -0800

    refactored FB auth serverside logic

--- node_app/index.js
+++ node_app/index.js
@@ -4,18 +4,17 @@ const express = require('express');
 const app = express();
 const exphbs = require('express3-handlebars');
 const bodyParser = require('body-parser');
-/* Start Code Change*/
 const session = require('express-session');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook');
-/* End Code Change*/
+
 // Database Models
 const db = require('./db/db.js');
 const User = require('./db/user.js');
 const Ride = require('./db/ride.js');
 
 app.use(express.static('public'));
-app.engine( 'hbs', exphbs({
+app.engine('hbs', exphbs({
 	extname: 'hbs',
 	defaultLayout: 'base',
 	layoutsDir: __dirname + '/views/layouts/',
@@ -23,49 +22,47 @@ app.engine( 'hbs', exphbs({
 }));
 app.set('view engine', 'hbs');
 
-/* Start Code Change*/
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 var facebook_id = '125963121407912';
 var facebook_secret_id = '867830b1fd7c4f910c171f06a1c0d3c7';
-
-var fbObject = {
+passport.use(new FacebookStrategy({
 	clientID: facebook_id,
 	clientSecret: facebook_secret_id,
 	callbackURL: 'http://localhost:3000/auth/facebook/callback',
-	profileFields: ['emails ']
-}
+	profileFields: ['id', 'displayName', 'email']
+},
+	function (accessToken, refreshToken, profile, done) {
+		// TODO save new user?
+		return done(null, profile);
+	}
+));
 
-var fbCallback = function(accessToken, refreshToken, profile, cb) {
-	console.log(accessToken, refreshToken, profile);
-};
+passport.serializeUser(function (user, done) {
+	done(null, user);
+});
 
-passport.use(new FacebookStrategy(fbObject, fbCallback));
-
-app.route('/auth')
-	.get(passport.authenticate('facebook', {scope: ['email']}));
-
-app.route('/auth/facebook/callback')
-	.get(passport.authenticate('facebook', function(err, user, info) {
-			console.log(err, user, info)
-			// save the data to a database (mongodb)
-	})
-);
-
-/* End Code Change*/
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
+passport.deserializeUser(function (user, done) {
+	done(null, user);
+});
 
 app.listen(settings.port, () => {
 	console.log("Listening on port 3000");
 });
 
 app.get('/', (req, res) => {
-	res.render('home', {
-		title: "Home"
-	});
+	if (req.user) {
+		res.redirect('/user/' + req.user.id);
+	} else {
+		res.render('home', {
+			title: "Home"
+		});
+	}
 });
-
 
 app.get('/ride/new', (req, res) => {
 	res.render('create_ride');
@@ -81,5 +78,19 @@ app.post('/ride/create', (req, res) => {
 	let rideDescription = req.body.rideDescription;
 	let ridePrice = req.body.price;
 	// TODO: Create and save Ride object to database
+	res.redirect('/');
+});
+
+app.get('/error', (req, res) => {
+	res.send("There was an error.");
+});
+
+// TODO make DB query for user after saving
+app.get('/user/:id', (req, res) => {
+	res.send(req.params.id);
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/error' }), (req, res) => {
 	res.redirect('/');
 });
